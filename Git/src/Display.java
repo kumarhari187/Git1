ce04f94b57159b664c45145709beb73b7d92687c
GitUser <git@user.com>
2016-03-14 19:55:43 +0530

    Initial Checkin.

---
+++ Git/src/Display.java
@@ -0,0 +1,14 @@
+
+public class Display {
+
+	
+	int data [] = {1,2,3};
+
+	public void display() {
+ 
+		System.out.println("This is Display.java");
+ 
+	}
+	
+	
+}
---
+++ Git/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	  public static void main(String[] args) {
+			
+			Display data = new Display();
+			
+			displayGreeting();
+			data.display();
+		
+		}
+		
+		private static void displayGreeting(){
+			
+			System.out.println("This is Main.java");
+			
+			
+		}
+		
+	
+}
